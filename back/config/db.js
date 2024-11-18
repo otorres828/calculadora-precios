6a3803591c9e865ec42bac64c6f75c01c52fdc92
otorres828 <olivertorres1997@gmail.com>
2024-11-18 07:31:12 -0600

    ajsutes

--- back/config/db.js
+++ back/config/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'default',
-  password: 'h7ADso9RXESZ',
-  host: 'ep-red-art-34547297-pooler.us-east-1.aws.neon.tech',
-  port: 5432, // default Postgres port
-  database: 'deliciouscake'
+  user: 'postgres',
+  password: 'qQiPDsekcCMcTTVM',
+  host: 'db.lamlaonpeyttjmfjzcsd.supabase.co',
+  port: 6543, // default Postgres port
+  database: 'postgres',
 });
 
 module.exports = {
