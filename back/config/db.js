94da2385eac281c61b80323bc977b0c43e6f3f32
otorres828 <olivertorres1997@gmail.com>
2024-11-19 08:41:44 -0600

    ajuste

--- back/config/db.js
+++ back/config/db.js
@@ -1,11 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user:     process.env.deliciouscake_USER,
+  user: process.env.deliciouscake_USER,
   password: process.env.deliciouscake_PASSWORD,
-  host:     process.env.deliciouscake_HOST,
-  port:     5432, // default Postgres port
+  host: process.env.deliciouscake_HOST,
+  port: 5432,
   database: process.env.deliciouscake_DATABASE,
+  ssl: {
+    rejectUnauthorized: false // Descomenta esto solo en entornos de desarrollo
+  }
 });
 
 module.exports = {
