70a59e632ef5fb43139eeb1cfd52cfe5853724e7
otorres828 <olivertorres1997@gmail.com>
2024-11-19 08:48:14 -0600

    ajuste

--- back/config/database.js
+++ back/config/database.js
@@ -1,10 +1,12 @@
 var Sequelize = require('sequelize').Sequelize;
 
-const sequelize = new Sequelize('costos', 'root', '', {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.deliciouscake_DATABASE, process.env.deliciouscake_USER, process.env.deliciouscake_PASSWORD, {
+  host: process.env.deliciouscake_HOST,
   dialect: 'postgres', // Especificamos que usamos PostgreSQL
   port: 5432, // Puerto por defecto de PostgreSQL
-  logging: console.log // Log all SQL queries
+  ssl: {
+    rejectUnauthorized: false // Descomenta esto solo en entornos de desarrollo
+  }
 });
 
 module.exports = sequelize;
